9ed9d3195c32544d0e51b6573c03ff9bde2ed4b7
Hamza <sterbenrk987@gmail.com>
2024-02-03 11:22:25 +0100

    Update server.js to listen on dynamic port

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ console.log(process.env.FRONTEND_URL);
 const dbURL = process.env.DB_URL;
 mongoose.connect(dbURL)
     .then(() => {
-        app.listen(3300)
+        app.listen(process.env.PORT||3300)
         console.log('Connected to DB')
     })
     .catch(err => console.log(err));
