1b34a60ecbf1e3b1812feac3d33ba9c95bd0088f
rezighamza <119344553+rezighamza@users.noreply.github.com>
2024-02-08 17:52:11 +0100

    Refactor CORS configuration in server.js

--- server.js
+++ server.js
@@ -7,16 +7,9 @@ const env = require('dotenv').config();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-const corsOptions = {
-    origin: [process.env.FRONTEND_URL],
-    Headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': process.env.FRONTEND_URL,
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-}};
 
-app.use(cors(corsOptions));
+
+app.use(cors());
 console.log(process.env.FRONTEND_URL);
 
 const dbURL = process.env.DB_URL;
